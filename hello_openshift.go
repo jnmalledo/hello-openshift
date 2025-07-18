be28bb47e84bec0c47bb8d6383f0d53a533c84ee
jnmalledo <jnmalledo@gmail.com>
2025-07-18 20:30:07 +0200

    Update hello_openshift.go

--- hello_openshift.go
+++ hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift!"
+		response = "See you later, alligator!"
 	}
 
 	fmt.Fprintln(w, response)
